bb173fa03dcb07050d6649cbe2ff51aab2de4289
anayet34 <anayetsarker40@gmail.com>
2023-02-13 20:48:11 +0600

    .

--- models/product.js
+++ models/product.js
@@ -1,32 +1,38 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
   name: {
+    type: String,
+    required: true,
+  },
+  company: {
     type: String,
     required: true,
   },
   price: {
     type: Number,
-    required: [true, "price must be provided"],
+    required: true,
+  },
+  colors: [String],
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
   },
   featured: {
     type: Boolean,
-    default: false,
-  },
-  rating: {
-    type: Number,
-    default: 4.9,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  company: {
-    type: String,
-    enum: {
-      values: ["apple", "samsung", "dell", "mi"],
-      message: `{VALUE} is not supported`,
-    },
+    required: true,
   },
 });
 
